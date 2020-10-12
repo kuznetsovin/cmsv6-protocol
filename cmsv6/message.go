cc699b2b081ddcfcefd17a76827611547cf6cdab
kuznetsovin <kuznetsovin@gmail.com>
2020-10-12 22:06:36 +0300

    create function for building response packet

--- cmsv6/message.go
+++ cmsv6/message.go
@@ -3,6 +3,7 @@ package cmsv6
 import (
 	"errors"
 	"strings"
+	"time"
 )
 
 type Decoder interface {
@@ -29,6 +30,22 @@ func ParsePacket(packet string) (interface{}, error) {
 
 	err = result.Decode(m)
 	return result, err
+}
+
+func CreateResponse(reqHeader Header, respTime time.Time, extra []string) string {
+	resp := C100{
+		Header: Header{
+			MessageID:    "dc0056",
+			PacketNumber: reqHeader.PacketNumber,
+			Type:         "C100",
+			DeviceID:     reqHeader.DeviceID,
+			Timestamp:    respTime,
+		},
+		RequestType:      reqHeader.Type,
+		RequestTimestamp: reqHeader.Timestamp,
+		ExtraFields:      extra,
+	}
+	return resp.Encode()
 }
 
 func parseMsg(msg string) ([]string, error) {
--- cmsv6/message_test.go
+++ cmsv6/message_test.go
@@ -19,3 +19,17 @@ func TestParsePacket(t *testing.T) {
 		}
 	}
 }
+
+func TestCreateResponse(t *testing.T) {
+	v101 := "$$dc0242,1,V101,0900000,,200924 112940,V0000,37,57,432623999,-0,0,0,0.00,0,0000000000007383,0000000000000000,0.00,0.00,0.00,0,0.00,0,0|0.00|0|0|0|0|0|0|0|0.00|0|0,,V1.0.0.1,4108,,0,0,0,SZ88888,1,USER=root,13,1,37,14,0900000,V2018 0414,V6.1.48 20180122,,0,1,0,#"
+	dt := time.Date(2020, time.September, 24, 11, 29, 41, 0, time.UTC)
+
+	r, err := ParsePacket(v101)
+	if assert.NoError(t, err) {
+		v, ok := r.(*V101)
+		if assert.Equal(t, ok, true) {
+			r := CreateResponse(v.Header, dt, []string{"0", "1", "1"})
+			assert.Equal(t, r, "$$dc0056,1,C100,0900000,,200924 112941,V101,200924 112940,0,1,1,#")
+		}
+	}
+}
