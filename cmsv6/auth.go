4b78b8ab1764fe3132497be69e35063b27c5669a
kuznetsovin <kuznetsovin@gmail.com>
2020-10-12 22:50:22 +0300

    add sub auth section v141

--- cmsv6/auth.go
+++ cmsv6/auth.go
@@ -25,3 +25,25 @@ func (v *V101) Decode(msg Message) error {
 	v.UnknownFields = msg[13:]
 	return nil
 }
+
+type V141 struct {
+	Header
+	CommonGPS
+	UnknownFields []string
+}
+
+func (v *V141) Decode(msg Message) error {
+	if len(msg) < 13 {
+		return errors.New("Incorrect message len.")
+	}
+	if err := v.Header.Decode(msg[:6]); err != nil {
+		return err
+	}
+
+	if err := v.CommonGPS.Decode(msg[6:13]); err != nil {
+		return err
+	}
+
+	v.UnknownFields = msg[13:]
+	return nil
+}
--- cmsv6/auth_test.go
+++ cmsv6/auth_test.go
@@ -21,3 +21,18 @@ func TestV101_Decode(t *testing.T) {
 		assert.Equal(t, v.Latitude, 37.96201733330556)
 	}
 }
+
+func TestV141_Decode(t *testing.T) {
+	cmd := "$$dc0146,3,V141,0900000,,200924 112940,V0000,-0,0,0,-0,0,0,0.00,0,0000000000007383,0000000000000000,0.00,0.00,0.00,0,0.00,0,0|0.00|0|0|0|0|0|0|0|0.00|0|0,#"
+	m, err := parseMsg(cmd)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	v := V141{}
+	if assert.NoError(t, v.Decode(m)) {
+		assert.Equal(t, v.MessageID, "dc0146")
+		assert.Equal(t, v.Type, "V141")
+		assert.Equal(t, len(v.UnknownFields), 12)
+	}
+}
