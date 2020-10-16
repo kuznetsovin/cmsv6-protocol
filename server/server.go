09c2e0ba13d9b306d14d9dcd57bedd796ed8ab3e
kuznetsovin <kuznetsovin@gmail.com>
2020-10-16 12:14:36 +0300

    handler is made path of server struct for readless

--- server/server.go
+++ server/server.go
@@ -1,9 +1,12 @@
 package server
 
 import (
+	"cmsv6-protocol/cmsv6"
 	"cmsv6-protocol/store"
 	"github.com/sirupsen/logrus"
+	"io"
 	"net"
+	"time"
 )
 
 type Server struct {
@@ -23,8 +26,68 @@ func (s *Server) Start() error {
 		if c, err := l.Accept(); err != nil {
 			logrus.Errorf("Connection error %v", err)
 		} else {
-			go connHandler(c, s.db)
+			go s.connHandler(c)
 		}
+	}
+}
+
+func (s *Server) connHandler(c net.Conn) {
+	var (
+		response string
+	)
+	defer c.Close()
+	for {
+		buf := make([]byte, 1024)
+		readLen, err := c.Read(buf)
+		switch err {
+		case nil:
+			break
+		case io.EOF:
+			continue
+		default:
+			logrus.Error("Received error ", err)
+			return
+		}
+
+		rawMsg := string(buf[:readLen])
+		logrus.WithFields(logrus.Fields{
+			"msg": rawMsg,
+		}).Debug("Received packet")
+
+		msg, err := cmsv6.ParsePacket(rawMsg)
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"msg": rawMsg,
+			}).Error("Received error ", err)
+			return
+		}
+
+		switch m := msg.(type) {
+		case *cmsv6.V101:
+			response = cmsv6.CreateResponse(m.Header, time.Now().UTC(), []string{"0", "1", "1"})
+		case *cmsv6.V141:
+			response = cmsv6.CreateResponse(m.Header, time.Now().UTC(), []string{"0", "0", "0", "0", "", "", "0", "", "0", ""})
+		case *cmsv6.V114:
+			p := store.GeoPoint{DeviceID: m.DeviceID, NavTime: m.Timestamp, Lat: m.Latitude, Lon: m.Longitude}
+			if err := s.db.Save(p); err != nil {
+				logrus.Error("Error save geo data ", err)
+			}
+		default:
+			logrus.Error("Unknown type")
+			continue
+		}
+
+		if response != "" {
+			_, err = c.Write([]byte(response))
+			if err != nil {
+				logrus.Error("Send response error ", err)
+				return
+			}
+			logrus.WithFields(logrus.Fields{
+				"msg": response,
+			}).Debug("Send response packet")
+		}
+
 	}
 }
 
