aa1cfc4867deefda144ce0c53eac217cf29c316c
kuznetsovin <kuznetsovin@gmail.com>
2020-10-19 13:28:20 +0300

    add video response type

--- cmsv6/video_request.go
+++ cmsv6/video_request.go
@@ -1,6 +1,7 @@
 package cmsv6
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -40,4 +41,32 @@ func CreateVideoRequest(pkgNum int, date time.Time, deviceID, ip string) string 
 		SrvPort:    6602,
 	}
 	return c.Encode()
+}
+
+type V100 struct {
+	gpsInfo
+	RequestType      string
+	RequestTimestamp time.Time
+	ExtraFields      []string
+}
+
+func (v *V100) Decode(msg Message) error {
+	if len(msg) < 24 {
+		return errors.New("Incorrect V100 message len.")
+	}
+
+	err := v.gpsInfo.decode(msg[:24])
+	if err != nil {
+		return err
+	}
+
+	v.RequestType = msg[25]
+
+	v.RequestTimestamp, err = time.Parse(timestampFmt, msg[26])
+	if err != nil {
+		return fmt.Errorf("Incorrect v100 timestamp format: %v", err)
+	}
+
+	v.ExtraFields = msg[27:]
+	return err
 }
--- cmsv6/video_request_test.go
+++ cmsv6/video_request_test.go
@@ -33,4 +33,21 @@ func TestCreateVideoRequest(t *testing.T) {
 		"$$dc0067,6,C508,0900000,,200924 113137,44108896,1,0,1,0,192.168.32.117,6602#")
 }
 
-//$$dc0192,6,V100,0900000,,200924 113137,A0000,37,57,432905999,55,49,240083999,0.00,0,0F0EE331000D7383,0000000000000000,0.00,0.00,0.00,0,0.00,0,0|0.00|0|0|0|0|0|0|0|0.00|0|0,,C508,200924 113137,0,1,1,,0#
+func TestV100_Decode(t *testing.T) {
+	cmd := "$$dc0192,6,V100,0900000,,200924 113137,A0000,37,57,432905999,55,49,240083999,0.00,0,0F0EE331000D7383,0000000000000000,0.00,0.00,0.00,0,0.00,0,0|0.00|0|0|0|0|0|0|0|0.00|0|0,,C508,200924 113137,0,1,1,,0#"
+	m, err := parseMsg(cmd)
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	v := V100{}
+	if assert.NoError(t, v.Decode(m)) {
+		assert.Equal(t, v.MessageID, "dc0192")
+		assert.Equal(t, v.Type, "V100")
+		assert.Equal(t, v.Timestamp, time.Date(2020, time.September, 24, 11, 31, 37, 0, time.UTC))
+		assert.Equal(t, v.Latitude, 37.962025166638895)
+		assert.Equal(t, v.RequestType, "C508")
+		assert.Equal(t, v.RequestTimestamp, time.Date(2020, time.September, 24, 11, 31, 37, 0, time.UTC))
+		assert.Equal(t, len(v.ExtraFields), 5)
+	}
+}
